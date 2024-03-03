156821a31296f486d4b3f1e92cf1dd2d9c0fa4f5
sergei-nefedov <post.fvyf6@slmail.me>
2024-03-03 16:46:03 +0400

    ref: modify classes for security

--- src/main/java/pers/nefedov/selsuptestapp/mappers/UserMapperImpl.java
+++ src/main/java/pers/nefedov/selsuptestapp/mappers/UserMapperImpl.java
@@ -1,15 +1,19 @@
 package pers.nefedov.selsuptestapp.mappers;
 
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import pers.nefedov.selsuptestapp.dto.UserCreationDto;
 import pers.nefedov.selsuptestapp.models.User;
 @Component
+@AllArgsConstructor
 public class UserMapperImpl implements UserMapper {
+    private final PasswordEncoder passwordEncoder;
     @Override
     public User mapToUser(UserCreationDto userCreationDto) {
         User user = new User();
         user.setLogin(userCreationDto.getLogin());
-        user.setPassword(userCreationDto.getPassword());
+        user.setPassword(passwordEncoder.encode(userCreationDto.getPassword()));
         user.setName(userCreationDto.getName());
         user.setDateOfBirth(userCreationDto.getDateOfBirth());
         user.setAccountBalance(userCreationDto.getAccountBalance());
@@ -20,7 +24,7 @@ public class UserMapperImpl implements UserMapper {
     public UserCreationDto mapToUserDto(User user) {
         UserCreationDto userDto = new UserCreationDto();
         userDto.setLogin(user.getLogin());
-        userDto.setPassword(user.getPassword());
+        userDto.setPassword(user.getPassword()); //TODO delete
         userDto.setName(user.getName());
         userDto.setDateOfBirth(user.getDateOfBirth());
         userDto.setAccountBalance(user.getAccountBalance());
--- src/main/java/pers/nefedov/selsuptestapp/models/User.java
+++ src/main/java/pers/nefedov/selsuptestapp/models/User.java
@@ -1,22 +1,25 @@
 package pers.nefedov.selsuptestapp.models;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Min;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.Check;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name="users")
-public class User {
+public class User implements UserDetails {
     @Id
     @Column(name="login")
     private String login;
@@ -34,4 +37,34 @@ public class User {
     @NotNull
     @Check(constraints = "account_balance >= 0")
     private double accountBalance;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
